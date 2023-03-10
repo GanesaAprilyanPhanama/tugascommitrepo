b0a654330cf387814a41ab2a8e654d5061d5aed7
Ganesa Phanama <ganesaap@gmail.com>
2023-03-10 10:17:12 +0700

    v1.0 buat program python dengan structure dictionary sesuai soal

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+user = {
+    
+    "user 1" : {
+    "nama": "arsene lupin", 
+    "nik" : 1234, 
+    "jenis_kelamin": "male",
+    "tanggal_lahir": "1902-03-23"
+    },
+    "user 2" : {
+    "nama": "sherlock holmes", 
+    "nik": 4321, 
+    "jenis_kelamin": "male",
+    "tanggal_lahir":"1876-08-16",
+    },
+    "user 3": {
+    "nama": "irene adler", 
+    "nik" : 6789, 
+    "jenis_kelamin": "female",
+    "tanggal_lahir": "1884-10-07"
+    }
+}
