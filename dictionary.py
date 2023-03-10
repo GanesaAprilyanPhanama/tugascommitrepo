60cc7d741df4fc41a3d451ede53fb7bb4a2a1b12
Ganesa Phanama <ganesaap@gmail.com>
2023-03-10 10:49:26 +0700

    v1.1 menerima inputan user serta mengubah struktur dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,21 +1,18 @@
-user = {
-    
-    "user 1" : {
-    "nama": "arsene lupin", 
-    "nik" : 1234, 
-    "jenis_kelamin": "male",
-    "tanggal_lahir": "1902-03-23"
-    },
-    "user 2" : {
-    "nama": "sherlock holmes", 
-    "nik": 4321, 
-    "jenis_kelamin": "male",
-    "tanggal_lahir":"1876-08-16",
-    },
-    "user 3": {
-    "nama": "irene adler", 
-    "nik" : 6789, 
-    "jenis_kelamin": "female",
-    "tanggal_lahir": "1884-10-07"
-    }
+user = {  
+    "nama": ["arsene lupin", "sherlock holmes" , "irene adler"], 
+    "nik" : [1234, 4321, 6789], 
+    "jenis_kelamin": ["male","male","female"],
+    "tanggal_lahir": ["1902-03-23", "1876-08-16","1884-10-07"],
 }
+
+nama = [input("masukkan nama anda: ")]
+nik = [input("Masukkan NIK Anda: ")]
+jenis_kelamin = [input("Masukkan jenis kelamin Anda: ")]
+tanggal_lahir = [input("Masukkan tanggal lahir Anda (format: yyyy-mm-dd): ")]
+
+user["nama"] += nama
+user["nik"] += nik
+user["jenis_kelamin"] += jenis_kelamin
+user["tanggal_lahir"] += tanggal_lahir
+
+print(user['nama'])
